6ce466058c3391359ff291bca6a7795c3064b769
Hoang Nam <nam@example.com>
2026-04-21 01:45:01 +0700

    fix nested repo

--- WMS/obj/Debug/net8.0/WMS.AssemblyInfo.cs
+++ WMS/obj/Debug/net8.0/WMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217cdfdbabef5473921252a58a437715c49b50bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
