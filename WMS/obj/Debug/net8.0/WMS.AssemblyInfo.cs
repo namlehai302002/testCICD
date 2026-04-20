1121150f9bb3af0da910a68f19ccddf538a918c1
Hoang Nam <nam@example.com>
2026-04-21 02:04:59 +0700

    update tests

--- WMS/obj/Debug/net8.0/WMS.AssemblyInfo.cs
+++ WMS/obj/Debug/net8.0/WMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ce466058c3391359ff291bca6a7795c3064b769")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4debb1038edac14b10e73d2aa4d5bd8359652d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
